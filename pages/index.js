ed44dc8a5b37d93d6e48aaa073b2eead54a6c35d
Sarim Abbas <sarim.stays@gmail.com>
2020-10-25 20:28:54 -0400

    simplify home page

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@
 import { getMdxContent } from "../utils";
 import Navbar from "../components/Navbar";
 
-const Home = ({ posts = [] }) => {
+const Home = () => {
   const mdxContent = getMdxContent();
   return (
     <div style={{ padding: "50px" }}>
@@ -17,53 +17,9 @@ const Home = ({ posts = [] }) => {
         ))}
       </ul>
 
-      {JSON.stringify(posts)}
+      {JSON.stringify(mdxContent)}
     </div>
   );
-};
-
-export const getStaticProps = async () => {
-  // const MDX_EXT = ".mdx";
-
-  // const getSlug = (p) => {
-  //   const containingFolder = PATH.dirname(p);
-  //   const basename = PATH.basename(p, MDX_EXT);
-  //   // index.mdx needs to return the containing folder
-  //   if (basename === "index") {
-  //     return PATH.join(containingFolder); // do join to get rid of inconsistencies
-  //   }
-  //   // otherwise, return path to file without extension
-  //   return PATH.join(containingFolder, basename);
-  // };
-
-  // const pathToPagesDirectory = PATH.join(process.cwd(), "pages");
-  // const allFilePaths = await READDIR(pathToPagesDirectory);
-  // const mdxFilePaths = allFilePaths
-  //   .filter((p) => PATH.extname(p) === MDX_EXT)
-  //   .map((p) => PATH.relative(pathToPagesDirectory, p));
-  // console.log(mdxFilePaths);
-  // const mdxModules = await Promise.all(
-  //   mdxFilePaths.map(async (p) => import(`pages/${p}`))
-  // );
-  // const mdxContent = mdxModules.map((m, i) => {
-  //   const slug = getSlug(mdxFilePaths[i]);
-  //   return {
-  //     slug,
-  //     meta: {
-  //       title: PATH.basename(slug),
-  //       // override with user-provided meta
-  //       ...(m?.meta ?? {}),
-  //     },
-  //   };
-  // });
-
-  // const mdxContent = getMdxContent();
-
-  return {
-    props: {
-      posts: [],
-    },
-  };
 };
 
 export default Home;
