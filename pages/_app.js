7fdd94ccce64d8dc1fdca7aaa743650ba3d50aa1
Sarim Abbas <sarim.stays@gmail.com>
2020-10-28 19:23:25 -0400

    test persistent playback by keeping some components in the _app.js level

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import "../global.css";
 import "normalize.css";
 import { GlobalStateProvider } from "hooks/useGlobalState";
+import CurrentlyPlaying from "components/CurrentlyPlaying";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <GlobalStateProvider>
+      {/* <div> */}
+      {/* <CurrentlyPlaying /> */}
       <Component {...pageProps} />
+      {/* </div> */}
     </GlobalStateProvider>
   );
 };
