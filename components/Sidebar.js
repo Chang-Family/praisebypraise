1bd0e18fa2159902e3699d2c2e13e2d978ee49f5
Sarim Abbas <sarim.stays@gmail.com>
2020-10-30 01:49:02 -0400

    open sidebar whenever track plays

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,10 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import useGlobalState from "hooks/useGlobalState";
 
 const Sidebar = ({ children }) => {
   const MIN_WIDTH = 10;
   const MAX_WIDTH = 100;
 
+  const { isPlaying } = useGlobalState();
   const [width, setWidth] = useState(MIN_WIDTH);
+
+  useEffect(() => {
+    if (isPlaying) {
+      setWidth(MAX_WIDTH);
+    }
+  }, [isPlaying]);
 
   const adjustWidth = (width) => {
     setWidth(width);
