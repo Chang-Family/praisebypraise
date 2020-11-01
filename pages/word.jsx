b4ccd571c8412942be1f5c5634e4c17490d1cc97
Sarim Abbas <sarim.stays@gmail.com>
2020-10-31 21:39:38 -0400

    get blog posts in word page

--- pages/word.jsx
+++ pages/word.jsx
@@ -1,8 +1,10 @@
 import styled from "styled-components";
 import Navbar from "components/Navbar";
 import Footer from "components/Footer";
+import { getMdxContent } from "utils";
 
 const Word = () => {
+  const blogPosts = getMdxContent().filter((p) => p?.meta?.type === "blog");
   return (
     <div style={{ margin: "5%" }}>
       <Navbar />
@@ -10,6 +12,9 @@ const Word = () => {
       <h1>2016</h1>
       <h1>2017</h1>
       <h1>2018</h1>
+      {blogPosts.map((p) => (
+        <h2>{p?.meta?.title}</h2>
+      ))}
       <Footer />
     </div>
   );
